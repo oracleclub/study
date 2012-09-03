0fcb20bd24da78a4b8ef6997de2793d705696582
Sun-woong, Jang <sily20@gmail.com>
2012-09-03 14:19:47 +0900

    spring-project

---
+++ spring-project/src/main/java/com/gurubee/study/emp/bo/EmpBO.java
@@ -0,0 +1,32 @@
+/*
+ * EmpBO.java 2012. 8. 10.
+ *
+ * Copyright oracleclub.com All rights Reserved.
+ */
+package com.gurubee.study.emp.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gurubee.study.emp.dao.EmpDAO;
+import com.gurubee.study.emp.model.Emp;
+
+/**
+ * Class 내용 기술
+ * 
+ * @author : oramaster
+ * 
+ */
+@Service
+public class EmpBO {
+
+    @Autowired
+    private EmpDAO empDAO;
+
+    public List<Emp> getEmpList() {
+        return empDAO.selectEmpList();
+    }
+
+}
